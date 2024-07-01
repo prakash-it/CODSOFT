4d92034aef6ced4fa22bb0a7957b047688d63615
prakash-it <prakashkandasamy4@gmail.com>
2024-07-01 07:41:14 +0530

    Intership

---
+++ Level-2/Task-1/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const paragraphs = document.querySelectorAll('p.hidden');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    paragraphs.forEach(paragraph => {
+        observer.observe(paragraph);
+    });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
